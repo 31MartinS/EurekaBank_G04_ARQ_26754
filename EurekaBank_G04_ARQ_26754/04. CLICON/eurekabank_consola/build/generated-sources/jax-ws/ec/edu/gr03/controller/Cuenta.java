f5b25ab49ec822d98e3d0d6f32d14d3f4898240f
Martin Miloslav Suquillo Tigasi <116769944+31MartinS@users.noreply.github.com>
2026-01-06 13:58:36 -0500

    Correccion de documentacion

---
+++ EurekaBank_G04_ARQ_26754/04. CLICON/eurekabank_consola/build/generated-sources/jax-ws/ec/edu/gr03/controller/Cuenta.java
@@ -0,0 +1,160 @@
+
+package ec.edu.gr03.controller;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para cuenta complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="cuenta"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="estado" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="moneda" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="nombreCliente" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="numeroCuenta" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="saldo" type="{http://www.w3.org/2001/XMLSchema}double"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "cuenta", propOrder = {
+    "estado",
+    "moneda",
+    "nombreCliente",
+    "numeroCuenta",
+    "saldo"
+})
+public class Cuenta {
+
+    protected String estado;
+    protected String moneda;
+    protected String nombreCliente;
+    protected String numeroCuenta;
+    protected double saldo;
+
+    /**
+     * Obtiene el valor de la propiedad estado.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * Define el valor de la propiedad estado.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEstado(String value) {
+        this.estado = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad moneda.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMoneda() {
+        return moneda;
+    }
+
+    /**
+     * Define el valor de la propiedad moneda.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMoneda(String value) {
+        this.moneda = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad nombreCliente.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    /**
+     * Define el valor de la propiedad nombreCliente.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNombreCliente(String value) {
+        this.nombreCliente = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad numeroCuenta.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    /**
+     * Define el valor de la propiedad numeroCuenta.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNumeroCuenta(String value) {
+        this.numeroCuenta = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad saldo.
+     * 
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * Define el valor de la propiedad saldo.
+     * 
+     */
+    public void setSaldo(double value) {
+        this.saldo = value;
+    }
+
+}
